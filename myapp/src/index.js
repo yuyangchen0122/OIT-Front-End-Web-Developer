5402c10017f3e0ff3391fcba9709d000b16ea2c0
yuyang chen <yuyangchen@yuyangs-MBP-2.home>
2019-02-28 20:46:39 -0500

    adding the app layout component

--- myapp/src/index.js
+++ myapp/src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import PrimarySearchAppBar from './component/AppBar';
+import PrimarySearchAppBar from './component/Header';
+import Album from './component/AppLayout'
 
 ReactDOM.render(<PrimarySearchAppBar />, document.querySelector('#root'));
+ReactDOM.render(<Album />, document.querySelector('#root'));
