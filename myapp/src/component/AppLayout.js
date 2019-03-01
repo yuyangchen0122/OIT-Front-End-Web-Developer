8e26d8e31fa0b157fc6035627b94257c262d5c18
yuyang chen <yuyangchen@yuyangs-MBP-2.home>
2019-03-01 03:22:55 -0500

    add web ui design

--- myapp/src/component/AppLayout.js
+++ myapp/src/component/AppLayout.js
@@ -4,6 +4,56 @@ import PropTypes from 'prop-types';
 import VirtualDraggableGrid from 'react-virtual-draggable-grid';
 import {withStyles} from "@material-ui/core";
 
+
+const styles = theme => ({
+    appBar: {
+        position: 'relative',
+    },
+    icon: {
+        marginRight: theme.spacing.unit * 2,
+    },
+    heroUnit: {
+        backgroundColor: theme.palette.background.paper,
+    },
+    heroContent: {
+        maxWidth: 600,
+        margin: '0 auto',
+        padding: `${theme.spacing.unit * 8}px 0 ${theme.spacing.unit * 6}px`,
+    },
+    heroButtons: {
+        marginTop: theme.spacing.unit * 4,
+    },
+    layout: {
+        width: 'auto',
+        marginLeft: theme.spacing.unit * 3,
+        marginRight: theme.spacing.unit * 3,
+        [theme.breakpoints.up(1100 + theme.spacing.unit * 3 * 2)]: {
+            width: 1100,
+            marginLeft: 'auto',
+            marginRight: 'auto',
+        },
+    },
+    cardGrid: {
+        padding: `${theme.spacing.unit * 8}px 0`,
+    },
+    card: {
+        height: '100%',
+        display: 'flex',
+        flexDirection: 'column',
+    },
+    cardMedia: {
+        paddingTop: '56.25%', // 16:9
+    },
+    cardContent: {
+        flexGrow: 1,
+    },
+    footer: {
+        backgroundColor: theme.palette.background.paper,
+        padding: theme.spacing.unit * 6,
+    },
+});
+
+
 const ItemComponent = props => {
     const { name, styles } = props;
 
@@ -67,7 +117,7 @@ class Grid extends React.Component {
 
         const item = {
             fixedWidth: 200,
-            fixedHeight: 100,
+            fixedHeight: 200,
             ItemComponent,
             itemProps: {
                 styles: {
@@ -121,8 +171,8 @@ class Grid extends React.Component {
                     getItems={this.getItems}
                 />
             </div>
-        );
+        )
     }
 }
 
-export default Grid;
+export default withStyles(styles)(Grid);
--- myapp/src/index.js
+++ myapp/src/index.js
@@ -3,5 +3,7 @@ import ReactDOM from 'react-dom';
 import PrimarySearchAppBar from './component/Header';
 import Grid from './component/AppLayout'
 
-ReactDOM.render(<PrimarySearchAppBar />, document.querySelector('#root'));
-ReactDOM.render(<Grid />, document.querySelector('#root'));
+ReactDOM.render(
+    <div><PrimarySearchAppBar/>,
+        <Grid /></div>,document.querySelector('#root')
+);
